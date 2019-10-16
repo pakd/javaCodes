ff4be0067c5ccf7c8907bdf8dd02d15d6d1086b1
pakd <deepak.kumar.cse12@gmail.com>
2019-10-17 00:51:57 +0530

    basic java trie testing

---
+++ Trie/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Trie test1 = new Trie();
+        test1.test_trie_basics();
+    }
+}
