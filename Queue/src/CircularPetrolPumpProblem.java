cd88fc0099067f54d5e349d119dff1862ac55f36
pakd <deepak.kumar.cse12@gmail.com>
2020-10-31 14:49:11 +0530

    Update CircularPetrolPumpProblem.java

--- Queue/src/CircularPetrolPumpProblem.java
+++ Queue/src/CircularPetrolPumpProblem.java
@@ -1,3 +1,28 @@
+/*
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        
+		
+		int start = 0;
+		
+		int n = gas.length;
+		int tank = 0;
+		int deficit = 0;
+		for(int i=0; i<n; i++) {
+			
+			tank += (gas[i] - cost[i]);
+			if(tank < 0) {
+				deficit += tank;
+				tank = 0;
+				start = i+1;
+			}
+		}
+		
+		return (deficit + tank >= 0)? start: -1;
+    }
+}
+*/
+
 import java.util.LinkedList;
 import java.util.Queue;
 
