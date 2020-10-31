3ac967782929e657dca062b7b5fda2f08c8dd69a
pakd <deepak.kumar.cse12@gmail.com>
2020-10-31 18:50:26 +0530

    Update MaximumProductSubArray.java

--- Array/src/MaximumProductSubArray.java
+++ Array/src/MaximumProductSubArray.java
@@ -1,3 +1,28 @@
+/*
+class Solution {
+    public int maxProduct(int[] nums) {
+		
+		int result = nums[0];
+		int curr_max = nums[0];
+		int curr_min = nums[0];
+
+		
+		
+		for(int i=1; i<nums.length; i++) {
+			
+			int curr_max_prev = curr_max;
+			
+			curr_max = Math.max(nums[i], Math.max(nums[i]*curr_max, nums[i]*curr_min));
+			curr_min = Math.min(nums[i], Math.min(nums[i]*curr_max_prev, nums[i]*curr_min));
+			
+			result = Math.max(curr_max, result);
+				
+		}
+		
+		return result;
+    }
+}
+*/
 public class MaximumProductSubArray {
 
     public int maxProduct(int[] arr) {
